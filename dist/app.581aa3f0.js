042e497cf9e4b2fc133664249baa1676212ea96b
AH <ah@h109-225-80-225.cust.a3fiber.se>
2020-02-10 22:05:14 +0100

    merged alan-dev

--- dist/app.581aa3f0.js
+++ dist/app.581aa3f0.js
@@ -99472,11 +99472,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-<<<<<<< HEAD
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52367" + '/');
-=======
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59719" + '/');
->>>>>>> alan-dev
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59518" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
