56cfb709b7d929a6219709e100f8e25c1c919d6b
BawsAlan <alan@ding.se>
2020-03-16 22:01:30 +0100

    whatever

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const publicPath = path.join(__dirname, ".", "public");
-const port = 1234;
+const port = 5000;
 
 app.use(express.static(publicPath));
 app.get("*", (req, res) => {
