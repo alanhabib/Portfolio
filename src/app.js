ca54560265d749b59e12b7c27bed7c15b914b498
BawsAlan <alan@ding.se>
2020-03-16 18:56:21 +0100

    took away parcel and installed webpack instead

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from "./Routers/AppRouter";
-import './styles/styles.scss';
+import './styles/styles.scss'
 
-ReactDOM.render(<AppRouter/>, document.getElementById('app'));
+ReactDOM.render(<AppRouter/>, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: './src/app.js',
+	devServer: {
+		historyApiFallback: true
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'index_bundle.js',
+		publicPath: "/"
+	},
+	watchOptions: {
+		ignored: ["node_modules", "dist/main.min.js", ".idea"]
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js)$/,
+				use: 'babel-loader',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+				exclude: /node_modules/
+			},
+			{
+				test: /\.(jpg|png|woff|woff2|eot|ttf|svg)$/,
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							limit: 100000,
+						},
+					},
+				],
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					// Creates `style` nodes from JS strings
+					'style-loader',
+					// Translates CSS into CommonJS
+					'css-loader',
+					// Compiles Sass to CSS
+					'sass-loader',
+				],
+			},
+		]
+	},
+	mode: 'development',
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, 'public/index.html'),
+			filename: "index.html"
+		})
+	]
+
+};
