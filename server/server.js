8ae240f76cb6501cdf63bd0517afca15f82fddc3
BawsAlan <alan@ding.se>
2020-03-16 19:52:53 +0100

    added a Procfile

--- server/server.js
+++ server/server.js
@@ -2,13 +2,13 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const publicPath = path.join(__dirname, "..", "public");
-const port = process.env.PORT || 3000;
+const port = 1234;
 
 app.use(express.static(publicPath));
 app.get("*", (req, res) => {
 	res.sendFile(path.join(publicPath, "index.html"))
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
 	console.log("Server is up")
 });
