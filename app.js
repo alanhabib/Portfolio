cad3fc9ccdeb190ecc3267209e32bf6d73808140
BawsAlan <alan@ding.se>
2020-03-22 09:36:01 +0100

    took away node file

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppRouter from "./Routers/AppRouter";
-import './styles/styles.scss'
+import AppRouter from "./src/Routers/AppRouter";
+import './src/styles/styles.scss'
 
 ReactDOM.render(<AppRouter/>, document.getElementById('root'));
